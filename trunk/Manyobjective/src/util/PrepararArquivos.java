8a4e527807bad2d587849fd37d49e813b92fa922
andcarv <andcarv@3b4712dd-e854-0410-9fd3-e99944f7831d>
2011-10-03 20:16:00 +0000

    Adição dos problemas ZTD 1, 2 e 3

--- trunk/Manyobjective/src/util/PrepararArquivos.java
+++ trunk/Manyobjective/src/util/PrepararArquivos.java
@@ -5,7 +5,6 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintStream;
-import java.math.BigDecimal;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Iterator;
